e4d06622fd2d7c43494d033a17a26341afbd4dbf
Sharan <sharan.gohar@gmail.com>
2022-07-16 19:53:14 +0500

    all features complete except displaying images

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,20 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  id = '';
+  idData = '';
   title = 'frontend';
-
+  metaData = '';
   fileName = '';
 
-  constructor(private http: HttpClient) {}
-  onFileSelected(event: any){
-    const file:File = event.target.files[0]
-    if(file){
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    const upload$ = this.http.get(`http://localhost:3000/files`)
+    upload$.subscribe(
+      {
+        next: data => {
+          this.metaData = JSON.stringify(data)
+        },
+        error: error => {
+          this.metaData = JSON.stringify(error)
+        }
+      }
+    )
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0]
+    if (file) {
       this.fileName = file.name
       const formData = new FormData()
       formData.append("file", file)
       const upload$ = this.http.post("http://localhost:3000/upload", formData)
       upload$.subscribe()
     }
-
+  }
+  onSubmitId() {
+    console.log(this.id)
+    console.log("Submit ID button clicked")
+    if (this.id) {
+      const upload$ = this.http.get(`http://localhost:3000/files/${this.id}`)
+      upload$.subscribe(
+        {
+          next: data => {
+            this.idData = JSON.stringify(data)
+          },
+          error: error => {
+            this.idData = JSON.stringify(error)
+          }
+        }
+      )
+    }
   }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,16 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FileUploadComponent } from './file-upload/file-upload.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FileUploadComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
