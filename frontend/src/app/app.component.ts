3c6178f58b71ed0a0835c916526ac0e081e0e2df
Sharan <sharan.gohar@gmail.com>
2022-07-16 18:56:06 +0500

    file upload in progress

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  fileName = '';
+
+  constructor(private http: HttpClient) {}
+  onFileSelected(event: any){
+    const file:File = event.target.files[0]
+    if(file){
+      this.fileName = file.name
+      const formData = new FormData()
+      formData.append("file", file)
+      const upload$ = this.http.post("http://localhost:3000/upload", formData)
+      upload$.subscribe()
+    }
+
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploadComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
